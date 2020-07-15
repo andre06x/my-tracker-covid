d4d7b2348a718cd4525f4bd14e070dd2cf3d7f0a
Andre <andre06x@gmail.com>
2020-07-15 10:59:08 -0300

    adicionando margin

--- src/pages/Brasil/styles.js
+++ src/pages/Brasil/styles.js
@@ -19,6 +19,7 @@ export const Conteudo = styled.div`
     div{
         margin-left: 40px;
         padding: 90px 30px;
+        margin-top:3px;
         /* background-color:black; */
         /* border: solid 2px #5740b3; */
         background-color: rgb(133,98,2226,1);
@@ -43,6 +44,7 @@ export const Conteudo = styled.div`
             animation: ${rotate} linear 2s infinite;
         }
     `}
+
 
 
 `
