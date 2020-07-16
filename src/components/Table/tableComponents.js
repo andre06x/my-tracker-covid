52cbecac14609c1499506e2fc32b9b62355c95db
Andre <andre06x@gmail.com>
2020-07-16 10:20:03 -0300

    sizeTable

--- src/components/Table/tableComponents.js
+++ src/components/Table/tableComponents.js
@@ -97,7 +97,7 @@ export const Table = styled.table`
             overflow:scroll;
             max-width: 100px;
             ${props => props.sizeTable && css`
-                font-size: 0.68em;
+                font-size: 0.66em;
             `}
         }
 
